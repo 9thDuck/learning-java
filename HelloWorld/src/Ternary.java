f6c551e6b2b91882548b620360593c804f9746a1
7thclass <msai@live.co.uk>
2023-10-15 20:27:39 +0530

    ternary done

--- HelloWorld/src/Ternary.java
+++ HelloWorld/src/Ternary.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Ternary {
+public class Ternary {
+    public static void main(String[] args) {
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        String s= isDomestic ? "This car is domestic!" : "This car is not domestic!";
+        System.out.println(s);
+    }
 }
