1bc9ebabb4e111781c3c7f5bbfdb624b1ed0803e
7thclass <msai@live.co.uk>
2023-10-15 20:27:25 +0530

    if else done

--- HelloWorld/src/IfThen.java
+++ HelloWorld/src/IfThen.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class IfThen {
+public class IfThen {
+    public static void main(String[] args) {
+        boolean isAlien = false;
+//        if (!isAlien) {
+//            System.out.println("Is it not an alien!");
+//            System.out.println("And I am scared of aliens.");
+//        }
+
+        int topScore = 80;
+
+        if(topScore == 100){
+            System.out.println("You got the high score!");
+        }else{
+            System.out.println("Your score is --> " + topScore);
+        }
+    }
 }
