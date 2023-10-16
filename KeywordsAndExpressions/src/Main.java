91b0706fc5c7dde0710094f88189915f37e27f33
7thclass <msai@live.co.uk>
2023-10-16 09:34:26 +0530

    done

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = (100d * 1.609344d);
+
+        int highScore = 2000;
+        int health = 50;
+        if((health < 20) && (highScore < 1000)){
+            highScore -= 1000;
+        }
+        
+    }
+}
